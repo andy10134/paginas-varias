459f2604f0a822e7b7293419d5e927a504d434b0
andy10134 <andy1013@hotmail.com>
2020-05-15 22:37:33 -0300

    blur efect

--- unsj propuesta/js/nav.js
+++ unsj propuesta/js/nav.js
@@ -2,12 +2,13 @@ $(document).ready(() => {
 
     $(".hamburger").click(function (e) { 
         if ($(this).hasClass("is-active")) {
-
             $(this).removeClass('is-active');
+            $('.blur').css('filter', 'blur(0)');
         } else{
             $(this).addClass('is-active');
+            $('.blur').css('filter', 'blur(1.5rem)');
         }
-        
+ 
     });
     
 });
